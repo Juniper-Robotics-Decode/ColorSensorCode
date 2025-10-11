a272e7d8f86a0e5203ddbda4dd69bfdd0bd2dcb6
juniperrobotics <271018@sycamoreschools.org>
2025-10-11 12:44:43 -0400

    Error on the android environment import statment is standing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/colorSensorCVS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/colorSensorCVS.java
@@ -5,11 +5,9 @@ import android.os.Environment;
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.IMU;
 
 import com.opencsv.CSVWriter;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.sun.tools.jdeprscan.CSV;
 
 
 import org.slf4j.Logger;
@@ -19,34 +17,38 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 @TeleOp
 public class colorSensorCVS extends LinearOpMode {
     private RevColorSensorV3 colorSensor;
 
     private static final Logger log = LoggerFactory.getLogger(colorSensorCVS.class);
 
+    private ElapsedTime loopTimer;
+
     private File file;
     private FileWriter fileWriter;
     private CSVWriter csvWriter;
     private final ArrayList<String[]> dataArray = new ArrayList<String[]>();
+    private boolean addData;
 
     @Override
     public void runOpMode() {
         colorSensor = hardwareMap.get(RevColorSensorV3.class, "colorSensor");
 
-        file = new File(String.format("%s/FIRST/colorSensorDat.csv", Environment.getExternalStorageDirectory().getAbsolutePath()));
+        loopTimer = new ElapsedTime();
+
+        file = new File(String.format("%s/FIRST/colorSensorData.csv", Environment.getExternalStorageDirectory().getAbsolutePath()));
         try {
             fileWriter = new FileWriter(file);
         } catch (IOException e) {
-            throw new RuntimeExecption(e);
+            throw new RuntimeException(e);
         }
         csvWriter = new CSVWriter(fileWriter);
-
         dataArray.add(new String[]{"Red(red)", "Green(green)", "Blue(blue)", "Alpha(alpha)"});
         waitForStart();
 
         for (int i = 0; i < 5000; i++) {
+            loopTimer.reset();
             int red = colorSensor.red();
             int blue = colorSensor.blue();
             int green = colorSensor.green();
@@ -63,8 +65,9 @@ public class colorSensorCVS extends LinearOpMode {
             String Blue = blue + "";
             String Alpha = alpha + "";
 
-            if (addData)
+            if (addData) {
                 dataArray.add(new String[]{Red, Green, Blue, Alpha});
+            }
             }
         }
 
