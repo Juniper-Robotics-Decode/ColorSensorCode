b170ceb62a8c37a2a0bcf3923c24aaba040b5ab4
SidP1203 <sid.pudipeddi@gmail.com>
2025-10-19 11:36:44 -0400

    Changes to include all 3 color sensors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/colorSensorCVS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/colorSensorCVS.java
@@ -21,6 +21,8 @@ import java.util.ArrayList;
 @TeleOp
 public class colorSensorCVS extends LinearOpMode {
     private RevColorSensorV3 colorSensor1;
+    private RevColorSensorV3 colorSensor2;
+    private RevColorSensorV3 colorSensor3;
 
 
     private ElapsedTime loopTimer;
@@ -34,11 +36,13 @@ public class colorSensorCVS extends LinearOpMode {
     @Override
     public void runOpMode() {
         colorSensor1 = hardwareMap.get(RevColorSensorV3.class, "colorSensor1");
+        colorSensor2 = hardwareMap.get(RevColorSensorV3.class, "colorSensor2");
+        colorSensor3 = hardwareMap.get(RevColorSensorV3.class, "colorSensor3");
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
         loopTimer = new ElapsedTime();
 
-        file = new File(String.format("%s/FIRST/colorSensorData25.csv", Environment.getExternalStorageDirectory().getAbsolutePath()));
+        file = new File(String.format("%s/FIRST/Purple2.csv", Environment.getExternalStorageDirectory().getAbsolutePath()));
 
         try {
             fileWriter = new FileWriter(file);
@@ -47,7 +51,9 @@ public class colorSensorCVS extends LinearOpMode {
         }
 
         csvWriter = new CSVWriter(fileWriter);
-        dataArray.add(new String[]{"Red1(red1)", "Green1(green1)", "Blue1(blue1)", "Alpha1(alpha1)", "Distance1(distance1)"});
+        dataArray.add(new String[]{"Red1(red1)", "Green1(green1)", "Blue1(blue1)", "Alpha1(alpha1)", "Distance1(distance1)",
+                "Red2(red2)", "Green2(green2)", "Blue2(blue2)", "Alpha2(alpha2)", "Distance2(distance2)",
+                "Red3(red3)", "Green3(green3)", "Blue3(blue3)", "Alpha3(alpha3)", "Distance3(distance3)"});
         waitForStart();
         if (addData) {
             for (int i = 0; i < 100; i++) {
@@ -56,13 +62,33 @@ public class colorSensorCVS extends LinearOpMode {
                 int blue1 = colorSensor1.blue();
                 int green1 = colorSensor1.green();
                 int alpha1 = colorSensor1.alpha();
+                int red2 = colorSensor2.red();
+                int blue2 = colorSensor2.blue();
+                int green2 = colorSensor2.green();
+                int alpha2 = colorSensor2.alpha();
+                int red3 = colorSensor3.red();
+                int blue3 = colorSensor3.blue();
+                int green3 = colorSensor3.green();
+                int alpha3 = colorSensor3.alpha();
                 double distance1 = colorSensor1.getDistance(DistanceUnit.MM);
+                double distance2 = colorSensor2.getDistance(DistanceUnit.MM);
+                double distance3 = colorSensor3.getDistance(DistanceUnit.MM);
 
                 telemetry.addData("Red1:", red1);
                 telemetry.addData("Green1:", green1);
                 telemetry.addData("Blue1:", blue1);
                 telemetry.addData("Alpha1:", alpha1);
                 telemetry.addData("Distance1:", distance1);
+                telemetry.addData("Red2:", red2);
+                telemetry.addData("Green2:", green2);
+                telemetry.addData("Blue2:", blue2);
+                telemetry.addData("Alpha2:", alpha2);
+                telemetry.addData("Distance2:", distance2);
+                telemetry.addData("Red3:", red3);
+                telemetry.addData("Green3:", green3);
+                telemetry.addData("Blue3:", blue3);
+                telemetry.addData("Alpha3:", alpha3);
+                telemetry.addData("Distance3:", distance3);
                 telemetry.addData("i:", i);
                 telemetry.update();
 
@@ -71,10 +97,20 @@ public class colorSensorCVS extends LinearOpMode {
                 String Blue1 = blue1 + "";
                 String Alpha1 = alpha1 + "";
                 String Distance1 = distance1 + "";
+                String Red2 = red2 + "";
+                String Green2 = green2 + "";
+                String Blue2 = blue2 + "";
+                String Alpha2 = alpha2 + "";
+                String Distance2 = distance2 + "";
+                String Red3 = red3 + "";
+                String Green3 = green3 + "";
+                String Blue3 = blue3 + "";
+                String Alpha3 = alpha3 + "";
+                String Distance3 = distance3 + "";
 
-                dataArray.add(new String[]{Red1, Green1, Blue1, Alpha1, Distance1});
-
-
+                dataArray.add(new String[]{Red1, Green1, Blue1, Alpha1, Distance1,
+                        Red2, Green2, Blue2, Alpha2, Distance2,
+                        Red3, Green3, Blue3, Alpha3, Distance3});
             }
         }
         try {
