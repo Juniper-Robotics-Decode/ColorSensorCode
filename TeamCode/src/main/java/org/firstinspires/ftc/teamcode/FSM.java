1fdf46b84e8d9800fd818833950838279598c038
juniperrobotics <271018@sycamoreschools.org>
2025-11-25 17:19:39 -0500

    Normalization Works!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FSM.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.hardware.rev.RevTouchSensor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+public class FSM {
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/spindexMotorNormalization.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/spindexMotorNormalization.java
@@ -7,13 +7,12 @@ import com.qualcomm.hardware.rev.RevTouchSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Gamepad;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @TeleOp
-public class SpindexMotor extends LinearOpMode {
+public class spindexMotorNormalization extends LinearOpMode {
     private DcMotor spindexMotor;
     private RevColorSensorV3 colorSensor1;
     private RevColorSensorV3 colorSensor2;
@@ -30,9 +29,16 @@ public class SpindexMotor extends LinearOpMode {
     private boolean[] green = new boolean[3];
     private boolean[] purple = new boolean[3];
     private String[] detectedMotif = new String[3];
+    private boolean[] tcs = new boolean[3];
     private double d1;
     private double d2;
     private double d3;
+    int x = 0;
+    int y = 0;
+    int ball;
+    int noBall;
+    int pressedIndex;
+    double motorPower;
 
     @Override
     public void runOpMode() {
@@ -45,12 +51,19 @@ public class SpindexMotor extends LinearOpMode {
         spindexMotor = hardwareMap.get(DcMotor.class, "spindexMotor");
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
+        while(!TCS1.isPressed()){
+            spindexMotor.setPower(-0.16);
+        }
+        spindexMotor.setPower(0);
+
         waitForStart();
 
         while (opModeIsActive()) {
             detectedMotif[0] = colorDetector(colorSensor1);
             detectedMotif[1] = colorDetector(colorSensor2);
             detectedMotif[2] = colorDetector(colorSensor3);
+
+
             d1 = colorSensor1.getDistance(DistanceUnit.MM);
             d2 = colorSensor2.getDistance(DistanceUnit.MM);
             d3 = colorSensor3.getDistance(DistanceUnit.MM);
@@ -81,23 +94,22 @@ public class SpindexMotor extends LinearOpMode {
                 if (!hasPurple) {
                     gamepad1.rumble(500);
                 } else {
-                    spindexMotor.setPower(-0.16);
+                    spindexMotor.setPower(motorPower);
                 }
             }
-
             if (gamepad1.circle) {
                 boolean hasGreen = green[0] || green[1] || green[2];
                 if (!hasGreen) {
                     gamepad1.rumble(500);
-                }else {
-                    spindexMotor.setPower(-0.16);
+                } else {
+                    spindexMotor.setPower(motorPower);
                 }
             }
             if (gamepad1.triangle) {
                 if (!full[0] && !full[1] && !full[2]) {
                     gamepad1.rumble(500);
                 } else {
-                    spindexMotor.setPower(-0.16);
+                    spindexMotor.setPower(motorPower);
                 }
             }
 
@@ -119,6 +131,30 @@ public class SpindexMotor extends LinearOpMode {
                 telemetry.addData("Pocket 3 is", "Full and under SHOOTER");
             }
 
+            for (int j = 0; j < detectedMotif.length; j++) {
+                if ((detectedMotif[j] == greenStr) || (detectedMotif[j] == purpleStr)) {
+                    ball = j;
+                } else {
+                    noBall = j;
+                }
+            }
+            for (int k = 0; k < tcs.length; k++) {
+                if (tcs[k]) {
+                    pressedIndex = k;
+                }
+            }
+            x = pressedIndex - ball;
+            y = pressedIndex - noBall;
+
+            if (x>y) {
+               motorPower = -0.16;
+            }
+            if (x<y) {
+                motorPower = 0.16;
+            }
+            if (x==y){
+                motorPower = -0.16;
+            }
 
             telemetry.addData("purple[0]", purple[0]);
             telemetry.addData("purple[1]", purple[1]);
@@ -126,6 +162,8 @@ public class SpindexMotor extends LinearOpMode {
             telemetry.addData("green[0]", green[0]);
             telemetry.addData("green[1]", green[1]);
             telemetry.addData("green[2]", green[2]);
+            telemetry.addData("X", x);
+            telemetry.addData("Y", y);
             telemetry.addData("TCS1", TCS1.isPressed());
             telemetry.addData("TCS2", TCS2.isPressed());
             telemetry.addData("TCS3", TCS3.isPressed());
@@ -133,25 +171,28 @@ public class SpindexMotor extends LinearOpMode {
             telemetry.update();
         }
     }
+        public String colorDetector (RevColorSensorV3 cs){
+            int blue = cs.blue();
+            int green = cs.green();
+            int red = cs.red();
+            double dREAD = cs.getDistance(DistanceUnit.MM);
 
-    public String colorDetector(RevColorSensorV3 cs) {
-        int blue = cs.blue();
-        int green = cs.green();
-        int red = cs.red();
-        double dREAD = cs.getDistance(DistanceUnit.MM);
+            if (dREAD <= 20) {
+                if (green > red && green > blue) {
+                    return greenStr;
 
-        if (dREAD <= 20) {
-            if (green > red && green > blue) {
-                return greenStr;
-
-            } else if (blue > red && blue > green) {
-                return purpleStr;
+                } else if (blue > red && blue > green) {
+                    return purpleStr;
+                }
+            } else {
+                return emptyStr;
             }
-        } else {
-            return emptyStr;
+            return "";
         }
-        return "";
     }
+
+
+
 
 // Square = Shoot purple
 // Circle = Shoot green
@@ -160,4 +201,5 @@ public class SpindexMotor extends LinearOpMode {
 //TCS 1 = pocket1
 //TCS 2 = pocket
 //TCS 3 = pocket3
-}
+
+//Pocket 1=0, 2=1, 3=2
